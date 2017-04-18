90abfef73dbbc6e709a80ddc13c5ab3da911a135
Torsten Irländer <torsten.irlaender@googlemail.com>
2017-04-18 23:22:31 +0200

    Implemented basic method to receive messages
    Implemented encoders and decoders for messages. Finally implemented a basic
    PING command. Added tests.

--- pynunzen/node/node.py
+++ pynunzen/node/node.py
@@ -1,3 +1,60 @@
+import json
+import datetime
+from pynunzen.helpers import utcts
+
+
+def cmd_ping(data):
+    """Implenets a simple PING command. The message will return the current UTC timestamp
+
+    :data: Received data
+    :returns: Message
+
+    """
+    response = {}
+    response["type"] = "response"
+    response["success"] = True
+    response["data"] = utcts(datetime.datetime.utcnow())
+    return encode_json_msg(response)
+
+
+def recv(json_msg):
+    """Will parse the given message and triggers whatever action is
+    needed. Finally the message will return a response. If the message
+    con not be parsed a :class:MessageParseException will be raised.
+
+    :json_msg: TODO
+    :returns: TODO
+
+    """
+    msg = decode_json_msg(json_msg)
+    command = msg["command"]
+    func = CMD2FUNC.get(command)
+    if func:
+        return func(msg["data"])
+    else:
+        raise MessageParseException("Command {} not known".format(command))
+
+
+def decode_json_msg(json_msg):
+    try:
+        return json.loads(json_msg)
+    except:
+        raise MessageParseException("Message can not be parsed")
+
+
+def encode_json_msg(msg):
+    if not isinstance(msg, dict):
+        raise MessageParseException("Message can not be parsed")
+    try:
+        return json.dumps(msg)
+    except:
+        raise MessageParseException("Message can not be parsed")
+
+
+class MessageParseException(ValueError):
+    pass
+
+
 class Node(object):
 
     """A single node in the P2P network. Nodes are used to share updates
@@ -19,3 +76,8 @@ class Node(object):
         connections."""
         self.peers = []
         """List of peers this node is connected to"""
+
+
+CMD2FUNC = {
+    "ping": cmd_ping
+}
--- tests/test_node.py
+++ tests/test_node.py
@@ -9,8 +9,11 @@ Tests for `node` module.
 """
 
 import pytest
-
-from pynunzen.node.node import Node
+import datetime
+from pynunzen.node.node import (
+    Node, recv, MessageParseException,
+    encode_json_msg, decode_json_msg
+)
 
 
 @pytest.fixture
@@ -31,3 +34,42 @@ def test_node_init_address(nodeA):
 
 def test_node_init_peers(nodeA):
     assert nodeA.peers == []
+
+
+def test_encode_message():
+    msg = {}
+    msg["success"] = True
+    msg["type"] = "response"
+    json_msg = encode_json_msg(msg)
+    assert json_msg.find('"success": true') > -1
+
+
+def test_encode_message_fail():
+    with pytest.raises(MessageParseException):
+        encode_json_msg("Foo")
+
+
+def test_encode_message_fail2():
+    with pytest.raises(MessageParseException):
+        encode_json_msg({"Foo": datetime.datetime.now()})
+
+
+def test_decode_message():
+    msg = decode_json_msg('{"command": "Foo", "data": "Bar"}')
+    assert msg["command"] == "Foo"
+    assert msg["data"] == "Bar"
+
+
+def test_decode_message_fail():
+    with pytest.raises(MessageParseException):
+        decode_json_msg("Foo")
+
+
+def test_recv_unknown_command():
+    with pytest.raises(MessageParseException):
+        recv('{"command": "Foo", "data": "Bar"}')
+
+
+def test_recv_ping_command():
+    response = recv('{"command": "ping", "data": ""}')
+    assert response.find('"success": true') > -1

2bdd409329666f68d9bb12bacca88faa6375dc84
Torsten Irländer <torsten.irlaender@googlemail.com>
2017-04-20 22:20:11 +0200

    Added logging config.

--- pynunzen/cli.py
+++ pynunzen/cli.py
@@ -1,19 +1,38 @@
 # -*- coding: utf-8 -*-
 
 import click
+import logging
 from pynunzen.node.node import Node
 from pynunzen.config import DEFAULT_CONFIG_PATH, get_config, get_node_server_address
+
+LOGLEVEL = {
+    "0": logging.ERROR,
+    "1": logging.WARNING,
+    "2": logging.INFO,
+    "3": logging.DEBUG
+}
+
+log = logging.getLogger(__name__)
 
 
 @click.group()
 @click.option("--config",
               default=DEFAULT_CONFIG_PATH,
               help="Path to the configuration file.")
+@click.option("-v", "--verbose", count=True,
+              help="Set verbosity of the node.")
 @click.pass_context
-def main(ctx, config):
+def main(ctx, config, verbose):
     """Console script for pynunzen client."""
     ctx.obj = {}
     ctx.obj["CONFIG"] = get_config(config)
+
+    # Set verbosity depending on how often the user provides the "-v"
+    # option.
+    level = LOGLEVEL.get(str(verbose), logging.DEBUG)
+    logging.basicConfig(level=level,
+                        format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                        datefmt='%Y-%m-%d %H:%M')
 
 
 @click.command()

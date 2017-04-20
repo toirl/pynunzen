4b1f28a8507ebd7748731e4c60bc9171a13d8d98
Torsten Irländer <torsten.irlaender@googlemail.com>
2017-04-20 21:46:44 +0200

    Added config option to pynunzen command and start node

--- pynunzen/cli.py
+++ pynunzen/cli.py
@@ -1,18 +1,28 @@
 # -*- coding: utf-8 -*-
 
 import click
+from pynunzen.node.node import Node
+from pynunzen.config import DEFAULT_CONFIG_PATH, get_config, get_node_server_address
 
 
 @click.group()
-def main(args=None):
+@click.option("--config",
+              default=DEFAULT_CONFIG_PATH,
+              help="Path to the configuration file.")
+@click.pass_context
+def main(ctx, config):
     """Console script for pynunzen client."""
-    pass
+    ctx.obj = {}
+    ctx.obj["CONFIG"] = get_config(config)
 
 
 @click.command()
-def serve():
-    """Will start the node server"""
-    pass
+@click.pass_context
+def serve(ctx):
+    """Will start the node server."""
+    config = ctx.obj["CONFIG"]
+    address = get_node_server_address(config)
+    Node(address)
 
 
 main.add_command(serve)
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -23,7 +23,7 @@ def test_command_line_interface():
     assert result.exit_code == 0
     help_result = runner.invoke(cli.main, ['--help'])
     assert help_result.exit_code == 0
-    assert '--help  Show this message and exit.' in help_result.output
+    assert 'Show this message and exit.' in help_result.output
 
 
 def test_serve_command():
